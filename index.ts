5d6c30a7053a5547bd203ce1943243379d8e2254
ebad <zaidebad1234@gmail.com>
2024-07-04 21:14:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+import { log } from "console";
+import inquirer from "inquirer";
+
+const num = await inquirer.prompt([
+  { message: "Enter 1st number", type: "number", name: "first" },
+  { message: "Enter 2nd number", type: "number", name: "second" },
+  {
+    message: "enter the operation",
+    name: "operation",
+    type: "list",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
+]);
+if (num.operation==="addition"){
+    console.log(`the answer is : ${num.first+num.second}`);    
+}
+else if (num.operation==="subtraction"){
+    console.log(`the answer is : ${num.first-num.second}`);    
+}
+else if (num.operation==="multiplication"){
+    console.log(`the answer is : ${num.first*num.second}`)
+}
+else {
+    console.log(`the answer is : ${num.first/num.second}`)
+}
